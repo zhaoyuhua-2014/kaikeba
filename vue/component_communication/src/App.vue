a7a4971feaf8c83c6b3dbd75f4e451f3cdafd357
zyh <zhaoyuhua_2014@163.com>
2020-01-05 23:28:02 +0800

    预习视频rander联系

--- vue/component_communication/src/App.vue
+++ vue/component_communication/src/App.vue
@@ -9,17 +9,15 @@
       </p>
       <el-button>el-button</el-button>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
