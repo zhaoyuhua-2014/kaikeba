db0407136c43105e93a2857b11435a391c4644eb
zhaoyuhua <zhaoyuhua_2014@163.com>
2020-01-10 18:57:17 +0800

    router查看

--- vue/vueRouterStore/custom-router/src/ivew/about/index.vue
+++ vue/vueRouterStore/custom-router/src/ivew/about/index.vue
@@ -1,5 +1,11 @@
 <template>
    <div class="">
-       <h4>about</h4>
+        <h4>about</h4>
+        <div class="nav">
+            <router-link to="/a">a</router-link>
+            <router-link to="b">b</router-link>
+        </div>
+        <router-view></router-view>
    </div>
 </template>
+
--- vue/vueRouterStore/custom-router/src/kRouter/router-view-ivew.js
+++ vue/vueRouterStore/custom-router/src/kRouter/router-view-ivew.js
@@ -6,13 +6,38 @@ export default {
             current
         } = this.$router
         let com = null
-        if (current === '' || current === '/'){
-            let p = hasMap[''].redirect
-            current = p.slice(1)
+        let cpCurrent  = current
+
+
+
+        // 标记深度
+        
+
+
+
+
+
+
+        console.log(cpCurrent)
+        if (typeof cpCurrent === 'undefined') {
+            console.log('unde')
+            return h(com)
         }
+        if (cpCurrent === '' || cpCurrent === '/'){
+            let p = hasMap[''].redirect
+            cpCurrent = p.slice(1)
+        }
+        console.log(this.$router)
+        console.log(hasMap[cpCurrent])
         // h() component
-        com = hasMap[current].component 
+        if (hasMap[cpCurrent].component) {
+            com = hasMap[cpCurrent].component
+        } else if (hasMap[cpCurrent].redirect) {
+            cpCurrent = hasMap[cpCurrent].redirect
+            com = hasMap[cpCurrent].component
+        }
         console.log(com)
+        
         return h(com)
     }
 }
